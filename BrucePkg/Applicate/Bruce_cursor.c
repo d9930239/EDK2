7a272703816f169fb5f881253cdfc186c3fc049e
Bruce_Wang <bruce_wang@compal.com>
2020-01-03 16:49:53 +0800

    Prepare for build
    -Due to this branch build error, just push for cherry pick

--- BrucePkg/Applicate/Bruce_cursor.c
+++ BrucePkg/Applicate/Bruce_cursor.c
@@ -10,9 +10,23 @@
 #include <Library/UefiApplicationEntryPoint.h>
 
 #define ID L"Bruce"
+/*
+VOID
+WaitAnyKeyPress()
+{
+    EFI_STATUS      Status = EFI_SUCCESS;
+    UINTN           Index = 0;
+    EFI_INPUT_KEY   Key = {0};
 
-
-
+    while (TRUE) {
+        gBS->WaitForEvent(1, &gST->ConIn->WaitForKey, &Index);
+        Status = gST->ConIn->ReadKeyStroke(gST->ConIn, &Key);
+        if ((Key.UnicodeChar != 0) || (Key.ScanCode != 0)) 
+            break;
+    }
+    return ;
+}
+*/
 EFI_STATUS
 EFIAPI
 Bruce_cursor(
@@ -23,10 +37,18 @@ Bruce_cursor(
     EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut;
     EFI_SIMPLE_TEXT_OUTPUT_MODE      SavedConsoleMode;
 
+    UINTN                            Columns;
+    UINTN                            Rows;
+
     ConOut = SystemTable->ConOut;
     ConOut->QueryMode (ConOut, SavedConsoleMode.Mode, &Columns, &Rows);
-    ConOut->QUERY();
+    Print (L"QueryMode Columns = %d, Rows= %d\n",Columns,Rows);
 
+
+    //ConOut->SetCursorPosition (ConOut, Column, Row);
+    //Print (L"SetCursorPosition = %d\n",Columns);
+    //ConOut->OutputString (ConOut, ID);
+    //Print (L"OutputString = %d\n",Columns);
 
     return EFI_SUCCESS;
 }
